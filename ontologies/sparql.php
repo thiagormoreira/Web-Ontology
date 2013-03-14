86222567c57afa29e223b2d895a4985d7c851158
Thiago Moreira <loganguns@gmail.com>
2013-03-14 08:54:54 -0300

    Indent Code

--- ontologies/sparql.php
+++ ontologies/sparql.php
@@ -1,7 +1,8 @@
 <?php
+
 require_once('lib/LibRDF/LibRDF.php');
 
-function query($person){ 
+function query($person) {
     // All models, i.e. graphs, reside in a storage. This defaults to
     // memory.
     $store = new LibRDF_Storage();
@@ -12,261 +13,245 @@ function query($person){
     // should be easy to implement. Anyways, in this case we're
     // dealing with an RDF/XML document:
     $model->loadStatementsFromURI(
-            new LibRDF_Parser('rdfxml'),
-            'http://owl.dev/ontologies/rolemap.rdf');
-    
+            new LibRDF_Parser('rdfxml'), 'http://owl.dev/ontologies/rolemap.rdf');
+
     $prefix = " PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
                 PREFIX owl: <http://www.w3.org/2002/07/owl#>
                 PREFIX xsd: <http://www.w3.org/2001/XMLSchema#>
                 PREFIX kgd: <http://localhost/ontologies/rolemap.rdf#>";
-    
+
     // Create a SPARQL query
     $sparql = " SELECT ?role WHERE {
                     ?role kgd:is_role_of ?Person .
-                    ?Person kgd:hasfullname \"".$person."\"^^xsd:string 
+                    ?Person kgd:hasfullname \"" . $person . "\"^^xsd:string 
                 }";
     $query = new LibRDF_Query($prefix . $sparql, null, 'sparql');
 
     // Execute the query. The results of a SPARQL SELECT provide
     // array access by using the variables used in the query as keys:
     $query = $query->execute($model);
-    
-    foreach($query as $result){
-        $resultArr['post']['role'] = str_replace("_", " ",formatSet($result['role']));
-        
+
+    foreach ($query as $result) {
+        $resultArr['post']['role'] = str_replace("_", " ", formatSet($result['role']));
     }
-    
+
     $sparql = " SELECT ?context
 
                 WHERE {?context kgd:is_personcontext_of ?person .
-                ?person kgd:hasfullname \"".$person."\"^^xsd:string
+                ?person kgd:hasfullname \"" . $person . "\"^^xsd:string
                 }";
     $query = new LibRDF_Query($prefix . $sparql, null, 'sparql');
-    
+
     $query = $query->execute($model);
-    
-    foreach($query as $result){
+
+    foreach ($query as $result) {
         $resultArr['post']['context'] = formatSet($result['context']);
     }
-    
+
     $sparql = " SELECT ?know_what
                 WHERE {?know_what kgd:is_personknowwhat_of ?Person .
-                ?Person kgd:hasfullname \"".$person."\"^^xsd:string
+                ?Person kgd:hasfullname \"" . $person . "\"^^xsd:string
                 }";
-    
+
     $query = new LibRDF_Query($prefix . $sparql, null, 'sparql');
-    
+
     $query = $query->execute($model);
-    
-    foreach($query as $i => $result){
+
+    foreach ($query as $i => $result) {
         $resultArr['competence_known']['know_what'][$i] = formatSet($result['know_what']);
     }
-    
+
     $sparql = " SELECT ?know_how
                 WHERE {?know_how kgd:is_personknowhow_of ?Person .
-                ?Person kgd:hasfullname \"".$person."\"^^xsd:string
+                ?Person kgd:hasfullname \"" . $person . "\"^^xsd:string
                 }";
-    
+
     $query = new LibRDF_Query($prefix . $sparql, null, 'sparql');
-    
+
     $query = $query->execute($model);
-    
-    foreach($query as $i => $result){
+
+    foreach ($query as $i => $result) {
         $resultArr['competence_known']['know_how'][$i] = formatSet($result['know_how']);
     }
-    
+
     $sparql = " SELECT ?know_why
                 WHERE {?know_why kgd:is_personknowwhy_of ?Person .
-                ?Person kgd:hasfullname \"".$person."\"^^xsd:string
+                ?Person kgd:hasfullname \"" . $person . "\"^^xsd:string
                 }";
-    
+
     $query = new LibRDF_Query($prefix . $sparql, null, 'sparql');
-    
+
     $query = $query->execute($model);
-    
-    foreach($query as $i => $result){
+
+    foreach ($query as $i => $result) {
         $resultArr['competence_known']['know_why'][$i] = formatSet($result['know_why']);
     }
-    
+
     $sparql = " SELECT ?know_with
                 WHERE {?know_with kgd:is_personknowwith_of ?Person .
-                ?Person kgd:hasfullname \"".$person."\"^^xsd:string
+                ?Person kgd:hasfullname \"" . $person . "\"^^xsd:string
                 }";
-    
+
     $query = new LibRDF_Query($prefix . $sparql, null, 'sparql');
-    
+
     $query = $query->execute($model);
-    
-    foreach($query as $i => $result){
+
+    foreach ($query as $i => $result) {
         $resultArr['competence_known']['know_with'][$i] = formatSet($result['know_with']);
     }
-    
+
     $sparql = " SELECT ?know_who
                 WHERE {?know_who kgd:is_personknowwho_of ?Person .
-                ?Person kgd:hasfullname \"".$person."\"^^xsd:string
+                ?Person kgd:hasfullname \"" . $person . "\"^^xsd:string
                 }";
-    
+
     $query = new LibRDF_Query($prefix . $sparql, null, 'sparql');
-    
+
     $query = $query->execute($model);
-    
-    foreach($query as $i => $result){
+
+    foreach ($query as $i => $result) {
         $resultArr['competence_known']['know_who'][$i] = formatSet($result['know_who']);
     }
-    
+
     $sparql = " SELECT ?know_where
                 WHERE {?know_where kgd:is_personknowwhere_of ?Person .
-                ?Person kgd:hasfullname \"".$person."\"^^xsd:string
+                ?Person kgd:hasfullname \"" . $person . "\"^^xsd:string
                 }";
-    
+
     $query = new LibRDF_Query($prefix . $sparql, null, 'sparql');
-    
+
     $query = $query->execute($model);
-    
-    foreach($query as $i => $result){
+
+    foreach ($query as $i => $result) {
         $resultArr['competence_known']['know_where'][$i] = formatSet($result['know_where']);
     }
-    
+
     $sparql = " SELECT ?know_when
                 WHERE {?know_when kgd:is_personknowwhen_of ?Person .
-                ?Person kgd:hasfullname \"".$person."\"^^xsd:string
+                ?Person kgd:hasfullname \"" . $person . "\"^^xsd:string
                 }";
-    
+
     $query = new LibRDF_Query($prefix . $sparql, null, 'sparql');
-    
+
     $query = $query->execute($model);
-    
-    foreach($query as $i => $result){
+
+    foreach ($query as $i => $result) {
         $resultArr['competence_known']['know_when'][$i] = formatSet($result['know_when']);
     }
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
+
     $sparql = " SELECT ?know_what
 
                 WHERE {?know_what kgd:is_knowwhat_of ?Task .
                 ?Task kgd:is_task_of ?Activity .
                 ?Activity kgd:is_activity_of ?Role .
-                ?Role kgd:hasrolename \"".$resultArr['post']['role']."\"^^xsd:string
+                ?Role kgd:hasrolename \"" . $resultArr['post']['role'] . "\"^^xsd:string
                 }";
-    
+
     $query = new LibRDF_Query($prefix . $sparql, null, 'sparql');
-    
+
     $query = $query->execute($model);
-    
-    foreach($query as $i => $result){
+
+    foreach ($query as $i => $result) {
         $resultArr['competence_needed']['know_what'][$i] = formatSet($result['know_what']);
     }
-    
+
     $sparql = " SELECT ?know_how
 
                 WHERE {?know_how kgd:is_knowhow_of ?Task .
                 ?Task kgd:is_task_of ?Activity .
                 ?Activity kgd:is_activity_of ?Role .
-                ?Role kgd:hasrolename \"".$resultArr['post']['role']."\"^^xsd:string
+                ?Role kgd:hasrolename \"" . $resultArr['post']['role'] . "\"^^xsd:string
                 }";
-    
+
     $query = new LibRDF_Query($prefix . $sparql, null, 'sparql');
-    
+
     $query = $query->execute($model);
-    
-    foreach($query as $i => $result){
+
+    foreach ($query as $i => $result) {
         $resultArr['competence_needed']['know_how'][$i] = formatSet($result['know_how']);
     }
-    
+
     $sparql = " SELECT ?know_why
 
                 WHERE {?know_why kgd:is_knowwhy_of ?Task .
                 ?Task kgd:is_task_of ?Activity .
                 ?Activity kgd:is_activity_of ?Role .
-                ?Role kgd:hasrolename \"".$resultArr['post']['role']."\"^^xsd:string
+                ?Role kgd:hasrolename \"" . $resultArr['post']['role'] . "\"^^xsd:string
                 }";
-    
+
     $query = new LibRDF_Query($prefix . $sparql, null, 'sparql');
-    
+
     $query = $query->execute($model);
-    
-    foreach($query as $i => $result){
+
+    foreach ($query as $i => $result) {
         $resultArr['competence_needed']['know_why'][$i] = formatSet($result['know_why']);
     }
-    
+
     $sparql = " SELECT ?know_with
 
                 WHERE {?know_with kgd:is_knowwith_of ?Task .
                 ?Task kgd:is_task_of ?Activity .
                 ?Activity kgd:is_activity_of ?Role .
-                ?Role kgd:hasrolename \"".$resultArr['post']['role']."\"^^xsd:string
+                ?Role kgd:hasrolename \"" . $resultArr['post']['role'] . "\"^^xsd:string
                 }";
-    
+
     $query = new LibRDF_Query($prefix . $sparql, null, 'sparql');
-    
+
     $query = $query->execute($model);
-    
-    foreach($query as $i => $result){
+
+    foreach ($query as $i => $result) {
         $resultArr['competence_needed']['know_with'][$i] = formatSet($result['know_with']);
     }
-    
+
     $sparql = " SELECT ?know_who
 
                 WHERE {?know_who kgd:is_knowwho_of ?Task .
                 ?Task kgd:is_task_of ?Activity .
                 ?Activity kgd:is_activity_of ?Role .
-                ?Role kgd:hasrolename \"".$resultArr['post']['role']."\"^^xsd:string
+                ?Role kgd:hasrolename \"" . $resultArr['post']['role'] . "\"^^xsd:string
                 }";
-    
+
     $query = new LibRDF_Query($prefix . $sparql, null, 'sparql');
-    
+
     $query = $query->execute($model);
-    
-    foreach($query as $i => $result){
+
+    foreach ($query as $i => $result) {
         $resultArr['competence_needed']['know_who'][$i] = formatSet($result['know_who']);
     }
-    
+
     $sparql = " SELECT ?know_where
 
                 WHERE {?know_where kgd:is_knowwhere_of ?Task .
                 ?Task kgd:is_task_of ?Activity .
                 ?Activity kgd:is_activity_of ?Role .
-                ?Role kgd:hasrolename \"".$resultArr['post']['role']."\"^^xsd:string
+                ?Role kgd:hasrolename \"" . $resultArr['post']['role'] . "\"^^xsd:string
                 }";
-    
+
     $query = new LibRDF_Query($prefix . $sparql, null, 'sparql');
-    
+
     $query = $query->execute($model);
-    
-    foreach($query as $i => $result){
+
+    foreach ($query as $i => $result) {
         $resultArr['competence_needed']['know_where'][$i] = formatSet($result['know_where']);
     }
-    
+
     $sparql = " SELECT ?know_when
 
                 WHERE {?know_when kgd:is_knowwhen_of ?Task .
                 ?Task kgd:is_task_of ?Activity .
                 ?Activity kgd:is_activity_of ?Role .
-                ?Role kgd:hasrolename \"".$resultArr['post']['role']."\"^^xsd:string
+                ?Role kgd:hasrolename \"" . $resultArr['post']['role'] . "\"^^xsd:string
                 }";
-    
+
     $query = new LibRDF_Query($prefix . $sparql, null, 'sparql');
-    
+
     $query = $query->execute($model);
-    
-    foreach($query as $i => $result){
+
+    foreach ($query as $i => $result) {
         $resultArr['competence_needed']['know_when'][$i] = formatSet($result['know_when']);
     }
-    
-    
+
+
     $resultArr['competence_needed']["know_what"] = array_unique($resultArr['competence_needed']['know_what']);
     $resultArr['competence_needed']['know_how'] = array_unique($resultArr['competence_needed']['know_how']);
     $resultArr['competence_needed']['know_why'] = array_unique($resultArr['competence_needed']['know_why']);
@@ -274,13 +259,14 @@ function query($person){
     $resultArr['competence_needed']['know_who'] = array_unique($resultArr['competence_needed']['know_who']);
     $resultArr['competence_needed']['know_where'] = array_unique($resultArr['competence_needed']['know_where']);
     $resultArr['competence_needed']['know_when'] = array_unique($resultArr['competence_needed']['know_when']);
-    
+
     return $resultArr;
 }
 
-function formatSet($string){
-    $explodeArr = explode("<http://localhost/ontologies/rolemap.rdf#",$string);
+function formatSet($string) {
+    $explodeArr = explode("<http://localhost/ontologies/rolemap.rdf#", $string);
     $explodeArr = explode(">", $explodeArr[1]);
     return $explodeArr[0];
 }
+
 ?>
